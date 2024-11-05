b2c710f9c154ed0fe7d9cb0e8889b2dcf3c5e119
bp42s <140995694+bp42s@users.noreply.github.com>
2024-11-05 13:20:52 -0500

    added ba2 crate and filled out toml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#[allow(unused_import_braces, unused_imports)]
+use ba2::{
+    fo4::{Archive, ArchiveKey, ArchiveOptions, Chunk, File},
+    prelude::*,
+};
+#[allow(unused_imports)]
+use std::fs;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("[archive-version-patcher]");
 }
